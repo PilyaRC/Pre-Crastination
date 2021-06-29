2ac6cf411df125ce5b926dc63a1d902afe3ca684
Roman Piliavskyi <chernosliv.billy@gmail.com>
2021-06-29 23:48:49 +0300

    Initial Commit

---
+++ Pre-Crastination/Pre_CrastinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_CrastinationApp.swift
+//  Pre-Crastination
+//
+//  Created by PiliavskyiMBP on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pre_CrastinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
